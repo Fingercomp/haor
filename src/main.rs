abe62920339b9d9943f088194c319f4f52d74c92
Fingercomp <fingercomp@users.noreply.github.com>
2018-05-18 21:09:53 +0700

    Add templates and routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate rocket_contrib;
+
+use std::collections::HashMap;
+
+use rocket_contrib::Template;
+
+type TemplateContext = HashMap<String, String>;
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", &TemplateContext::new())
+}
+
+#[get("/setup/rust")]
+fn setup_rust() -> Template {
+    Template::render("setup-rust", &TemplateContext::new())
+}
+
+#[get("/setup/haor")]
+fn setup_haor() -> Template {
+    Template::render("setup-haor", &TemplateContext::new())
+}
+
+#[get("/copyright")]
+fn copyright() -> Template {
+    Template::render("copyright", &TemplateContext::new())
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index, setup_rust, setup_haor, copyright])
+        .attach(Template::fairing())
+        .launch();
 }
