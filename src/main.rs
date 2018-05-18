c76b803d72391c4cb9662edd9fc52e69ae860aa9
Fingercomp <fingercomp@users.noreply.github.com>
2018-05-18 22:14:45 +0700

    Add styles

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ extern crate rocket;
 extern crate rocket_contrib;
 
 use std::collections::HashMap;
+use std::path::{Path, PathBuf};
 
+use rocket::response::NamedFile;
 use rocket_contrib::Template;
 
 type TemplateContext = HashMap<String, String>;
@@ -30,9 +32,15 @@ fn copyright() -> Template {
     Template::render("copyright", &TemplateContext::new())
 }
 
+#[get("/static/<file..>")]
+fn static_files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, setup_rust, setup_haor, copyright])
+        .mount("/", routes![index, setup_rust, setup_haor, copyright,
+                            static_files])
         .attach(Template::fairing())
         .launch();
 }
